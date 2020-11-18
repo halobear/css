e9c1327b4a28b3a1079a15bd6ce6c0fe8c73121f
luzhongkuan <luzhongk@126.com>
2020-11-18 22:36:25 +0800

    feat: deplay

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 set -e
-cd dist
+cd html
 git init .
+echo "theme: jekyll-theme-cayman" > _config.yml
 git add .
 git commit -am 'deploy'
 git remote add origin https://github.com/halobear/css.git
-git push origin gh-pages -f
+git push origin master:gh-pages -f
+rm -fr .git
+cd -
